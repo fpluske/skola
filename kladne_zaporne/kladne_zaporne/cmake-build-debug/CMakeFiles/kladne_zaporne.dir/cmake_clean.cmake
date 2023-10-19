e02f3e19ba0973912bee1d75c64721ae18b9a2c0
Your Name <you@example.com>
2023-10-19 08:33:35 +0200

    Upload

---
+++ kladne_zaporne/kladne_zaporne/cmake-build-debug/CMakeFiles/kladne_zaporne.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kladne_zaporne.dir/main.c.obj"
+  "kladne_zaporne.exe"
+  "kladne_zaporne.exe.manifest"
+  "kladne_zaporne.pdb"
+  "libkladne_zaporne.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kladne_zaporne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sude_liche/sude_liche/cmake-build-debug/CMakeFiles/sude_liche.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sude_liche.dir/main.c.obj"
+  "libsude_liche.dll.a"
+  "sude_liche.exe"
+  "sude_liche.exe.manifest"
+  "sude_liche.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sude_liche.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
