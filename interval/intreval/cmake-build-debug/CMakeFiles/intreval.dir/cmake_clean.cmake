5b09af70b38d7bda96a831d9bcb7375f1550dd39
Your Name <you@example.com>
2023-10-25 08:19:39 +0200

    create

---
+++ interval/intreval/cmake-build-debug/CMakeFiles/intreval.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intreval.dir/main.c.obj"
+  "intreval.exe"
+  "intreval.exe.manifest"
+  "intreval.pdb"
+  "libintreval.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/intreval.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
