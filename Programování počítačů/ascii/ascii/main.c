21f3a7d0607d217ad3b608e4ad331c48fb09fa35
fpluske <frantisek.pluske@seznam.cz>
2023-10-31 12:27:17 +0100

    V 0.4

--- Programování počítačů/ascii/ascii/main.c
+++ Programování počítačů/ascii/ascii/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#define POCETPRVKU 256
-#define POCETSLOUPCU 8
+#define POCETPRVKU 256 //definuje konstantu POCETPRVKU
+#define POCETSLOUPCU 8 //definuje konstantu POCETSLOUPCU
+#define ZACATEK 14 //definuje konstantu ZACATEK
 
 int main() {
-    for (int i = 0; i < POCETPRVKU; ++i) {
-        printf("%4d %c",i,i);
+    int opakovani = 0;
+    for (int prvek = ZACATEK; prvek < POCETPRVKU; ++prvek) { //nastavi začátek pomocí konstanty ZACATEK, po každém opakování prvek++. Opakuje kód, pokud je prvek < POCETPRVKU
+        printf("%10d %c",prvek,prvek); //vypíše proměnnou prvek jako číslo, poté jako znak (tzn. znak ASCII)
+        opakovani++;
+        if(opakovani % POCETSLOUPCU ==0) {
+            printf("\n");
+        }
     }
     return 0;
 }
